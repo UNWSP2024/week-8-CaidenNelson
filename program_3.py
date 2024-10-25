367440121fb53b3288788f74ef37c1f36e20f801
CaidenNelson <cwnelson4@students.unwsp.edu>
2024-10-25 17:56:31 -0500

    Update program_3.py

--- program_3.py
+++ program_3.py
@@ -1,8 +1,29 @@
-# Program #3: Capital Quiz
-# Write a program that creates a dictionary containing the U.S. states as keys, 
-# and their capitals as values.  
-# The program should then randomly quiz the user by displaying the name of a state 
-# and asking the user to enter the state's capital.  
-# The program should count of the number of correct and incorrect responses.  
-# (You could alternatively use another country and provinces, 
-# or countries of the world and capitals).
+import random
+
+
+states = {'Minnesota':'St Paul', 'Washington':'Olympia','Alabama':'Montgomery','Alaska':'Juneau','Arizona':'Phoenix','Arkansas':'Little Rock',
+          'California':'Sacramento', 'Colorado':'Denver', 'Connecticut':'Hartford','Delaware':'Dover','Florida':'Tallahassee','Georgia':'Atlanta','Hawaii':'Honolulu',
+          'Idaho':'Boise','Illinois':'Springfield','Indiana':'Indianapolis','Iowa':'Des Moines','Kansas':'Topeka','Kentucky':'Frankfort','Louisiana':'Baton Rouge',
+          'Maine':'Augusta','Maryland':'Annapolis','Massachusetts':'Boston','Michigan':'Lansing','Mississippi':'Jackson','Missouri':'Jefferson City','Montana':'Helena',
+          'Nebraska':'Lincoln','Nevada':'Carson City','New Hampshire':'Concord','New Jersey':'Trenton','New Mexico':'Santa Fe','New York':'Albany','North Carolina':'Raleigh',
+          'North Dakota':'Bismark','Ohio':'Columbus','Oklahoma':'Oklahoma City','Oregon':'Salem','Pennsylvania':'Harrisburg','Rhode Island':'Providence','South Carolina':'Columbia',
+          'South Dakota':'Pierre','Tennessee':'Nashville','Texas':'Austin','Utah':'Salt Lake City','Vermont':'Montpelier','Virginia':'Richmond','West Virgina':'Charleston',
+          'Wisconsin':'Madison','Wyoming':'Cheyenne'  }
+
+total_correct = 0
+total_wrong = 0
+count = 0
+while count < 50:
+    count+=1
+    state = random.choice(list(states.keys()))
+    print(state)
+
+    answer = input('What is the capitol of this state? ').title()
+    if answer == states.get(state):
+        print('correct')
+        total_correct+=1
+    else:
+        print('wrong')
+        total_wrong+=1
+    del states[state]
+print('You got',total_correct,'right and',total_wrong,'wrong')
